8e7ff9ec7f9096abe2d140a29dc939c106a31304
Amadeusz <xaaq333@gmail.com>
2018-03-02 01:01:16 +0100

    add TODO

--- project_files/network_files/activation_functions.py
+++ project_files/network_files/activation_functions.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 
 import numpy
 
-# sprawdzic czy te funkcje (i ogolnie wsyzstkie inne jakie zrobilem) moga przyjmowac wielowymiarowe wektory i dobrze je zwracaja
+# TODO: sprawdzic czy te funkcje (i ogolnie wsyzstkie inne jakie zrobilem) moga przyjmowac wielowymiarowe wektory i dobrze je zwracaja
 class AbstractActivationFunction(ABC):
     """
     Base class for activation functions used in neural networks.
