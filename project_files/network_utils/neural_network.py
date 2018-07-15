946405c7e6da9956eba6f5951c5220434f9cf762
Xaaq <xaaq333@gmail.com>
2018-07-15 17:57:35 +0200

    add todo and modify existing one

--- project_files/network_utils/neural_network.py
+++ project_files/network_utils/neural_network.py
@@ -13,7 +13,8 @@ from project_files.network_utils.network_layers import FlatteningLayer, FullyCon
 # TODO: zrobic testy
 # TODO: jesli sie da to z klas wywalic inity i przerzucic zmienne inicjalizowane w nich do ciala klasy (OSTROŻNIE! to zmienne statyczne wtedy beda)
 # TODO: przeskanowac wszystko pylintem
-# TODO: wypisac typy argumentow i typy zwracane wszystkich funkcji (w docstringach tez)
+# TODO: zrobic type hinty i wyrzucic je z docstringow
+# TODO: sprawdzic czy nie ma lepszego sposobu na budowanie sieci niz uzywanie metod z NeuralNetwork
 # TODO: sprawdzic w calym projekcie komentarze (szczegolnie pod katem tego czy jest w nich slowo "image", ew. zastapic "data sample"
 # TODO: zamienic fory na list comprehension
 # TODO: dodac setery i getery za pomoca @property
