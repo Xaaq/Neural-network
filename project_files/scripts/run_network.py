6d081b14d2e1babe99ee734e1211450143243347
Xaaq <xaaq333@gmail.com>
2018-07-18 22:30:13 +0200

    clean up run script

--- project_files/scripts/run_network.py
+++ project_files/scripts/run_network.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-from project_files.network_utils.network_layers import FullyConnectedLayer
-from project_files.network_utils.neural_network import NeuralNetworkBuilder
-
-# train_data_x, train_data_y = create_train_data("images\\small_set", 50)
+from project_files.neural_network.activation_functions import SigmoidFunction
+from project_files.neural_network.network_layers import FullyConnectedLayer
+from project_files.neural_network.neural_network import NeuralNetworkBuilder
 
 neural_network = (NeuralNetworkBuilder()
-                  .add_layer(FullyConnectedLayer(10))
-                  .add_layer(FullyConnectedLayer(1))
+                  .add_layer(FullyConnectedLayer(10, SigmoidFunction))
+                  .add_layer(FullyConnectedLayer(2, SigmoidFunction))
                   .build((6,)))
 
 train_data_x = np.array([[1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
@@ -16,19 +15,17 @@ train_data_x = np.array([[1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
                          [0.0, 0.1, 0.2, 0.5, 0.6, 0.5],
                          [0.0, 0.0, 0.0, 0.3, 0.7, 0.4],
                          [0.0, 0.2, 0.1, 0.8, 0.9, 0.0]])
-train_data_y = np.array([[1], [1], [1], [0], [0], [0]])
+train_data_y = np.array([1, 1, 1, 0, 0, 0])
 
-test_data = np.array([[0.5, 0.2, 0.3, 0.1, 0.1, 0.0],
-                      [0.9, 0.9, 0.0, 0.0, 0.3, 0.2],
-                      [1, 1, 1, 0, 0, 0],
-                      [0, 0, 0, 1, 1, 1],
-                      [0.0, 0.0, 0.0, 0.9, 0.9, 0.9]])
-before = neural_network.predict(test_data)
-neural_network.teach_network(train_data_x, train_data_y)
-after = neural_network.predict(test_data)
+test_data_x = np.array([[0.5, 0.2, 0.3, 0.1, 0.1, 0.0],
+                        [0.9, 0.9, 0.0, 0.0, 0.3, 0.2],
+                        [1, 1, 1, 0, 0, 0],
+                        [0, 0, 0, 1, 1, 1],
+                        [0.0, 0.0, 0.0, 0.9, 0.9, 0.9]])
 
-print(before == np.array([[1], [1], [1], [0], [0]]))
-print(after == np.array([[1], [1], [1], [0], [0]]))
-# print(train_data_y)
-# n = numpy.array([[[3, 4, 8], [3, 55, 8]], [[99, 3, 2], [322, 2, 22]]])
-# print(n)
+before = neural_network.predict(test_data_x)
+neural_network.teach_network(train_data_x, train_data_y, 150)
+after = neural_network.predict(test_data_x)
+
+print(before)
+print(after)
