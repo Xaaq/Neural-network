d4f5f305b56991b283f70212364a331a240d5865
Xaaq <xaaq333@gmail.com>
2018-07-18 19:39:45 +0200

    add type hints and docstrings

--- project_files/utils/neural_network_progress_bar.py
+++ project_files/utils/neural_network_progress_bar.py
@@ -1,19 +1,35 @@
+"""
+This module contains progress bar used in neural network.
+"""
 import sys
 
 from tqdm import tqdm
 
 
 class NeuralNetworkProgressBar(tqdm):
+    """
+    This is wrapper class for tqdm progress bar.
+    """
     __column_width = 80
     __bar_format = "Learning progress: [{bar}]     Remaining time: {remaining}s     Cost: {desc}"
 
-    def __init__(self, iteration_count):
+    def __init__(self, iteration_count: int):
+        """
+        Initializes this progress bar.
+
+        :param iteration_count: number of iterations on which this progress bar will operate
+        """
         super().__init__(range(iteration_count),
                          file=sys.stdout,
                          ncols=self.__column_width,
                          bar_format=self.__bar_format)
 
     def update_cost(self, cost: float):
+        """
+        Updates cost function value in this progress bar description.
+
+        :param cost: cost function value
+        """
         formatted_cost = "{0:.4f}".format(cost)
         self.set_description_str(formatted_cost)
         self.ncols = self.__column_width + len(formatted_cost)
