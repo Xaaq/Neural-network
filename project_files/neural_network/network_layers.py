cab9f14edbed0a60e8751b470c19c21234bb9f3f
Xaaq <xaaq333@gmail.com>
2018-07-28 02:20:25 +0200

    Revert "change pass to return in flattening layer weights update"
    This reverts commit 84db9c2

--- project_files/neural_network/network_layers.py
+++ project_files/neural_network/network_layers.py
@@ -82,7 +82,7 @@ class FlatteningLayer(AbstractLayer):
         return multidimensional_data
 
     def update_weights(self, learning_rate: float):
-        return
+        pass
 
     @property
     def __output_neuron_count(self) -> int:
