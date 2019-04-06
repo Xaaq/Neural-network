1a45174d9c33a5648e712d6dfc01a17ba01edacf
xaaq <xaaq333@gmail.com>
2019-04-07 00:12:13 +0200

    put all code in script into a function

--- project_files/scripts/run_network.py
+++ project_files/scripts/run_network.py
@@ -5,28 +5,34 @@ from project_files.neural_network.activation_functions import SigmoidFunction
 from project_files.neural_network.network_layers import FullyConnectedLayer, FlatteningLayer
 from project_files.neural_network.neural_network import NeuralNetworkBuilder
 
-train_data_x = mnist.train_images()
-train_data_y = mnist.train_labels()
 
-test_data_x = mnist.test_images()
-test_data_y = mnist.test_labels()
+def main():
+    train_data_x = mnist.train_images()
+    train_data_y = mnist.train_labels()
 
-shape = train_data_x[0].shape
+    test_data_x = mnist.test_images()
+    test_data_y = mnist.test_labels()
 
-neural_network = (NeuralNetworkBuilder()
-                  .set_layers([FlatteningLayer(),
-                               FullyConnectedLayer(50, SigmoidFunction()),
-                               FullyConnectedLayer(50, SigmoidFunction()),
-                               FullyConnectedLayer(10, SigmoidFunction())])
-                  .build(shape))
-neural_network.fit(train_data_x, train_data_y, 100, learning_rate=1)
-after = neural_network.predict(train_data_x)
+    shape = train_data_x[0].shape
 
-print(after)
-print(train_data_y)
-print(np.sum(after == train_data_y) / len(after))
+    neural_network = (NeuralNetworkBuilder()
+                      .set_layers([FlatteningLayer(),
+                                   FullyConnectedLayer(50, SigmoidFunction()),
+                                   FullyConnectedLayer(50, SigmoidFunction()),
+                                   FullyConnectedLayer(10, SigmoidFunction())])
+                      .build(shape))
+    neural_network.fit(train_data_x, train_data_y, 100, learning_rate=1)
+    after = neural_network.predict(train_data_x)
 
-after = neural_network.predict(test_data_x)
-print(after)
-print(test_data_y)
-print(np.sum(after == test_data_y) / len(after))
+    print(after)
+    print(train_data_y)
+    print(np.sum(after == train_data_y) / len(after))
+
+    after = neural_network.predict(test_data_x)
+    print(after)
+    print(test_data_y)
+    print(np.sum(after == test_data_y) / len(after))
+
+
+if __name__ == "__main__":
+    main()
