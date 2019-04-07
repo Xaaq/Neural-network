e89ac03b3a41949b78bbae76a4efb1a32f48479e
xaaq <xaaq333@gmail.com>
2019-04-07 17:08:57 +0200

    add docstrings to run_network script and extract code into functions

--- project_files/scripts/run_network.py
+++ project_files/scripts/run_network.py
@@ -1,37 +1,65 @@
+"""
+Script that shows sample functionalities of `NeuralNetwork` class.
+"""
+from typing import Tuple
+
 import mnist
 import numpy as np
 
 from project_files.neural_network.activation_functions import SigmoidFunction
 from project_files.neural_network.network_layers import FullyConnectedLayer, FlatteningLayer
-from project_files.neural_network.neural_network import NeuralNetworkBuilder
+from project_files.neural_network.neural_network import NeuralNetworkBuilder, NeuralNetwork
 
 
-def main():
-    train_data_x = mnist.train_images()
-    train_data_y = mnist.train_labels()
+def build_network(shape: Tuple[int, ...]) -> NeuralNetwork:
+    """
+    Creates `NeuralNetwork` instance.
 
-    test_data_x = mnist.test_images()
-    test_data_y = mnist.test_labels()
-
-    shape = train_data_x[0].shape
-
+    :param shape: shape of single data sample
+    :return: built `NeuralNetwork` instance
+    """
     neural_network = (NeuralNetworkBuilder()
                       .set_layers([FlatteningLayer(),
                                    FullyConnectedLayer(50, SigmoidFunction()),
                                    FullyConnectedLayer(50, SigmoidFunction()),
                                    FullyConnectedLayer(10, SigmoidFunction())])
                       .build(shape))
+    return neural_network
+
+
+def predict_and_print_results(neural_network: NeuralNetwork, type_of_data: str, data_samples: np.ndarray,
+                              data_labels: np.ndarray):
+    """
+    Predicts given data using provided neural network and prints results.
+
+    :param neural_network: network to use
+    :param type_of_data: type of used data ("test" or "train")
+    :param data_samples: data samples
+    :param data_labels: data labels
+    """
+    predicted_data = neural_network.predict(data_samples)
+    number_of_correct_labels = np.sum(predicted_data == data_labels)
+    prediction_accuracy = number_of_correct_labels * 100 / len(predicted_data)
+    print(f"Accuracy on {type_of_data} data: {prediction_accuracy}%")
+
+
+def main():
+    """
+    Shows functionality of `NeuralNetwork` class.
+    """
+    train_data_x = mnist.train_images()[:1000]
+    train_data_y = mnist.train_labels()[:1000]
+
+    test_data_x = mnist.test_images()[:100]
+    test_data_y = mnist.test_labels()[:100]
+
+    shape = train_data_x[0].shape
+
+    neural_network = build_network(shape)
     neural_network.fit(train_data_x, train_data_y, 100, learning_rate=1)
-    after = neural_network.predict(train_data_x)
 
-    print(after)
-    print(train_data_y)
-    print(np.sum(after == train_data_y) / len(after))
-
-    after = neural_network.predict(test_data_x)
-    print(after)
-    print(test_data_y)
-    print(np.sum(after == test_data_y) / len(after))
+    predict_and_print_results(neural_network, "train", train_data_x, train_data_y)
+    predict_and_print_results(neural_network, "test", test_data_x, test_data_y)
 
 
 if __name__ == "__main__":
