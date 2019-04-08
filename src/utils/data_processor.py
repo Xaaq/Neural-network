8193ec7eafaca17fe4a90f92322f02e6c4332551
xaaq <xaaq333@gmail.com>
2019-04-08 22:45:47 +0200

    make DataProcessor remember first seen data coefficients and use them later

--- src/utils/data_processor.py
+++ src/utils/data_processor.py
@@ -1,9 +1,19 @@
 """
 Module containing class that provides tools to pre and post-process data fed to neural network.
 """
-from typing import Tuple
+from dataclasses import dataclass
+from typing import Optional
 
 import numpy as np
+
+
+@dataclass
+class Dataset:
+    """
+    Data class containing data and their labels in form of vectors for every data sample.
+    """
+    data: np.ndarray
+    label_matrix: np.ndarray
 
 
 class DataProcessor:
@@ -11,59 +21,54 @@ class DataProcessor:
     Class that provides tools to pre-process data that are input to neural network and post-process its output.
     """
 
-    @classmethod
-    def preprocess_data(cls, input_data: np.ndarray, label_vector: np.ndarray,
-                        number_of_labels: int) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Preprocesses input data by normalizing it and label vector by converting it to matrix.
+    def __init__(self):
+        self.__data_mean: Optional[np.ndarray] = None
+        self.__data_std_dev: Optional[np.ndarray] = None
+        self.__number_of_labels: Optional[int] = None
 
-        :param input_data: input data to normalize
-        :param label_vector: vector of labels to change to matrix
-        :param number_of_labels: number of possible labels
-        :return: normalized input data, matrix of labels
+    def normalize_data(self, data_to_normalize: np.ndarray) -> np.ndarray:
         """
-        normalized_data = cls.normalize_data(input_data)
-        label_matrix = cls.convert_label_vector_to_matrix(label_vector, number_of_labels)
-        return normalized_data, label_matrix
-
-    @staticmethod
-    def normalize_data(data_to_normalize: np.ndarray) -> np.ndarray:
-        """
-        Normalizes given matrix - transforms values in it to range [0, 1].
+        Normalizes given matrix - transforms values in it to range around [-1, 1]. When first used, it remembers data
+        normalization coefficients, so firstly use it on train set and then on test set.
 
         :param data_to_normalize: data to process
         :return: normalized data
         """
-        max_number = np.max(data_to_normalize)
-        min_number = np.min(data_to_normalize)
-        amplitude = max_number - min_number
-        normalized_data = (data_to_normalize - min_number) / amplitude
+        if self.__data_mean is None or self.__data_std_dev is None:
+            self.__data_mean = np.mean(data_to_normalize)
+            self.__data_std_dev = np.std(data_to_normalize)
+
+        normalized_data = (data_to_normalize - self.__data_mean) / self.__data_std_dev
         return normalized_data
 
-    @staticmethod
-    def convert_label_vector_to_matrix(label_vector: np.ndarray, number_of_labels: int) -> np.ndarray:
+    def convert_label_vector_to_matrix(self, label_vector: np.ndarray) -> np.ndarray:
         """
-        Converts vector of values (labels) to matrix representation. For example when 5 `number_of_labels` provided it
-        converts following vector:
+        Converts vector of labels to matrix representation. First time it is used, it deduces number of labels based on
+        highest data label and saves it for future calls. Deducing number of labels is based on value of highest label
+        value e.g. if highest label has value of 4, then output matrix will have 5 labels (because counting from 0).
+
+        For example when 5 labels will be deduced, it converts following vector:
         ::
-            [1, 3, 2, 1, 0]
+            [1, 3, 4, 1, 0]
 
         To following matrix:
         ::
             [[0, 1, 0, 0, 0],
              [0, 0, 0, 1, 0],
-             [0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 1],
              [0, 1, 0, 0, 0],
              [1, 0, 0, 0, 0]]
 
         :param label_vector: vector of labels
-        :param number_of_labels: number of possible data labels
         :return: matrix of labels
         """
+        if self.__number_of_labels is None:
+            self.__number_of_labels = label_vector.max() + 1
+
         label_matrix = []
 
         for label_value in label_vector:
-            row = np.zeros(number_of_labels)
+            row = np.zeros(self.__number_of_labels)
             row[label_value] = 1
             label_matrix.append(row)
 
